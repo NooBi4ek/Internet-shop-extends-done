4a0b33ffa2f2a7f764f5857291d86da3cefd0924
Maksim <gajdukmaksim710@gmail.com>
2023-02-03 22:07:57 +0300

    Starting learn react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { getByRole, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Upper_Header_input } from './styled/styled-header/SHeader';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('internet-shop-render', () => {
+  it('Render Header Component', () => {
+    const SetSearch = jest.fn();
+    const SetSearchModal = jest.fn();
+    const { container } = render(
+      <Upper_Header_input
+        type="checkbox"
+        placeholder="please,seach..."
+        onChange={(e) => SetSearch(e.target.value)}
+        onClick={() => SetSearchModal(false)}
+      />,
+    );
+    const check = container.firstChild;
+    expect(check).not.toBeChecked();
+    userEvent.click(check);
+    expect(check).toBeChecked();
+  });
 });
